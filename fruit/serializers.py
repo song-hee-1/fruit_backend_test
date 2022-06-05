2ec65075a30f6499d9d18a612d0fd1f8d19b4561
한송희 <98_han@naver.com>
2022-06-05 18:34:53 +0900

    delete all

--- fruit/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Fruit
-
-
-class FruitSerializers(serializers.ModelSerializer):
-    class Meta:
-        model = Fruit
-        fields = ('pk', 'name', 'season', 'description', 'image')
