4f3fad12e4b4bbc5f22275c716d6011e24b5f256
한송희 <98_han@naver.com>
2022-06-14 22:35:40 +0900

    Connect with postgresql database

--- fruitsapi/settings.py
+++ fruitsapi/settings.py
@@ -93,8 +93,12 @@ WSGI_APPLICATION = 'fruitsapi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'fruitsinfo',
+        'USER': get_secret("USER"),
+        'PASSWORD': get_secret("PASSWORD"),
+        'HOST': get_secret("HOST"),
+        'PORT': '5432',
     }
 }
 
