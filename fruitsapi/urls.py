82efa4a724aecf1b31efbba22d574bee9a116ea7
한송희 <98_han@naver.com>
2022-06-15 15:36:26 +0900

    Add home simple helloAPI

--- fruitsapi/urls.py
+++ fruitsapi/urls.py
@@ -17,9 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-
+from fruits.views import helloAPI
 
 urlpatterns = [
+    path('', helloAPI),
     path('fruits/', include('fruits.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
