54a3637320a8e309731b1c40c123f8adb91dad44
한송희 <98_han@naver.com>
2022-06-20 15:15:16 +0900

    Create index.html

--- fruits/views.py
+++ fruits/views.py
@@ -3,13 +3,17 @@ from rest_framework.decorators import api_view
 from .models import Info
 from .serializers import InfoSerializer
 from rest_framework import generics
-
+from django.shortcuts import render
 
 # Create your views here.
 
 @api_view(['Get'])
 def helloAPI(request):
-    return Response("Hello World!")
+    fruits_info_list = Info.objects.all()
+    context = {
+        'fruits_info_list' : fruits_info_list
+    }
+    return render(request, 'fruits/index.html', context)
 
 
 
